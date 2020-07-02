8d7ea57dde74262c9f7edb51fb65187e47ba7572
ANTHONYANDREWS <30433910+ANTHONYANDREWS@users.noreply.github.com>
2020-07-02 13:57:36 +0530

    Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import {RouterModule,Routes} from '@angular/router';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookListComponent } from './components/book-list/book-list.component';
+import { BookService } from './services/book.service';
+import { PageNotFoundComponent } from './comments/page-not-found/page-not-found.component';
+import { BookCategoryComponent } from './components/book-category/book-category.component';
 
+const routes: Routes=[
+  {path: 'books', component: BookListComponent},
+  {path: 'category/:id', component: BookListComponent},
+  {path: '', redirectTo:'/', pathMatch:'full'},
+  {path: '**', component: PageNotFoundComponent},
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookListComponent,
+    PageNotFoundComponent,
+    BookCategoryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [
+    BookService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
